9d2a374d34fe6b33e407e8a1252cca4490c128ab
Dinesh Kumar <68773181+Dineshchelani@users.noreply.github.com>
2022-02-25 15:53:48 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function countCurrency(event) {
 function calculateNotes(preferdNote, amount) {
     let notes = [500, 100, 50, 20, 10, 5, 1];
     //noteCounter used to store no of notes against each note in the array 'notes'
-    let noteCounter = Array(8).fill(0);
+    let noteCounter = Array(7).fill(0);
     
     //To calculate the number of preferd currency notes
     PNI = notes.indexOf(preferdNote) //PNI = Preferd Note Index
@@ -28,14 +28,20 @@ function calculateNotes(preferdNote, amount) {
             noteCounter[PNI] = 200
         }
     }
-    amount = amount - noteCounter[PNI] * notes[PNI];
+    amount = amount - (noteCounter[PNI] * notes[PNI]);
     
     //To calculate the number of currency notes
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 7; i++) {
         if (amount >= notes[i] && i !== PNI) {
             noteCounter[i] = Math.floor(amount / notes[i]);
             amount = amount - (noteCounter[i] * notes[i]);
         }
+    }
+    if(amount !==0 && notes[PNI] == 1){
+        x = 5-amount;
+        noteCounter[PNI]-=x;
+        i = notes.indexOf(5);
+        noteCounter[i]+=1
     }
     printNotes(notes,noteCounter)
 }
@@ -43,16 +49,17 @@ function calculateNotes(preferdNote, amount) {
 //function to display the total number of currency notes
 function printNotes(notes,noteCounter){
     // Print notes
-    const Div = document.getElementById("resultScreen");
-    Div.style.display = "block"
+    document.getElementById("resultScreen").style.display = "block";
+    const Div = document.getElementById("result");
+    Div.innerHTML = "";
     let h2 = Div.appendChild(document.createElement("h2"));
-    h2.innerText = `Currency Note : Number`
+    h2.innerText = `Currency Note : Number`;
 
     //Printing the calculated no of notes stored in the array noteCounter using loop
-    for (let i = 0; i < 8; i++) {
+    for (let i = 0; i < 7; i++) {
         if (noteCounter[i] != 0) {
-            let h4 = Div.appendChild(document.createElement("h4"));
-            h4.innerText = `${notes[i]}  :  ${noteCounter[i]} `
+            let h3 = Div.appendChild(document.createElement("h3"));
+            h3.innerText = `${notes[i]}  :  ${noteCounter[i]} `
         }
     }
 
